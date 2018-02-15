4d00f4d44ae73ad474da96a00449a8f860af608f
Your Name <you@example.com>
2018-02-15 20:38:52 +0100

    consumer

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using transfer;
+
+namespace consumer_transfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var c = new Class1();
+            c.A();
+        }
+    }
+}
